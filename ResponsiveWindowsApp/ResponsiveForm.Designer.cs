950b5bab1e325eee83aa6ad3d84eb7ed812a102b
Mona Lisa <sadik.fattah@outlook.com>
2018-05-09 10:24:01 +0100

    app

---
+++ ResponsiveWindowsApp/ResponsiveForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace ResponsiveWindowsApp
+{
+    partial class ResponsiveForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Responsivelabel = new System.Windows.Forms.Label();
+            this.ResponsiveTextBox = new System.Windows.Forms.TextBox();
+            this.ResponsiveDataGridView = new System.Windows.Forms.DataGridView();
+            this.ResponsiveButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ResponsiveDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Responsivelabel
+            // 
+            this.Responsivelabel.AutoSize = true;
+            this.Responsivelabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Responsivelabel.Location = new System.Drawing.Point(540, 114);
+            this.Responsivelabel.Name = "Responsivelabel";
+            this.Responsivelabel.Size = new System.Drawing.Size(221, 31);
+            this.Responsivelabel.TabIndex = 0;
+            this.Responsivelabel.Text = "Responsive label";
+            // 
+            // ResponsiveTextBox
+            // 
+            this.ResponsiveTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResponsiveTextBox.Location = new System.Drawing.Point(596, 333);
+            this.ResponsiveTextBox.Name = "ResponsiveTextBox";
+            this.ResponsiveTextBox.Size = new System.Drawing.Size(244, 38);
+            this.ResponsiveTextBox.TabIndex = 1;
+            // 
+            // ResponsiveDataGridView
+            // 
+            this.ResponsiveDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ResponsiveDataGridView.Location = new System.Drawing.Point(41, 74);
+            this.ResponsiveDataGridView.Name = "ResponsiveDataGridView";
+            this.ResponsiveDataGridView.Size = new System.Drawing.Size(465, 273);
+            this.ResponsiveDataGridView.TabIndex = 2;
+            // 
+            // ResponsiveButton
+            // 
+            this.ResponsiveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResponsiveButton.Location = new System.Drawing.Point(275, 439);
+            this.ResponsiveButton.Name = "ResponsiveButton";
+            this.ResponsiveButton.Size = new System.Drawing.Size(465, 72);
+            this.ResponsiveButton.TabIndex = 3;
+            this.ResponsiveButton.Text = "Click";
+            this.ResponsiveButton.UseVisualStyleBackColor = true;
+            // 
+            // ResponsiveForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(919, 724);
+            this.Controls.Add(this.ResponsiveButton);
+            this.Controls.Add(this.ResponsiveDataGridView);
+            this.Controls.Add(this.ResponsiveTextBox);
+            this.Controls.Add(this.Responsivelabel);
+            this.Name = "ResponsiveForm";
+            this.Text = "Responsive Form";
+            this.Load += new System.EventHandler(this.ResponsiveForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ResponsiveDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Responsivelabel;
+        private System.Windows.Forms.TextBox ResponsiveTextBox;
+        private System.Windows.Forms.DataGridView ResponsiveDataGridView;
+        private System.Windows.Forms.Button ResponsiveButton;
+    }
+}
+
---
+++ ResponsiveWindowsApp/ResponsiveForm.cs
@@ -0,0 +1,78 @@
+﻿//////////////////////////////////////////////
+//////////////////////////////////////////////
+// Coded by Mehedi Shams Rony ////////////////
+///////// October 2016 ///////////////////////
+//////////////////////////////////////////////
+//////////////////////////////////////////////
+using System;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ResponsiveWindowsApp.Classes;
+
+namespace ResponsiveWindowsApp
+{
+    public partial class ResponsiveForm : Form
+    {
+        Responsive ResponsiveObj;
+
+        public ResponsiveForm()
+        {
+            InitializeComponent();
+
+            ResponsiveObj = new Responsive(Screen.PrimaryScreen.Bounds);
+            ResponsiveObj.SetMultiplicationFactor();
+
+            SetupGrid();
+        }
+
+        private void ResponsiveForm_Load(object sender, EventArgs e)
+        {
+            Width = ResponsiveObj.GetMetrics(Width, "Width");    // Form width and height set up.
+            Height = ResponsiveObj.GetMetrics(Height, "Height");
+            Left = Screen.GetBounds(this).Width / 2 - Width / 2;  // Form centering.
+            Top = Screen.GetBounds(this).Height / 2 - Height / 2 - 30;  // 30 is a calibration factor.
+
+            foreach (Control Ctl in this.Controls)
+            {
+                Ctl.Font = new Font(FontFamily.GenericSansSerif, ResponsiveObj.GetMetrics((int)Ctl.Font.Size), FontStyle.Regular);
+                Ctl.Width = ResponsiveObj.GetMetrics(Ctl.Width, "Width");
+                Ctl.Height = ResponsiveObj.GetMetrics(Ctl.Height, "Height");
+                Ctl.Top = ResponsiveObj.GetMetrics(Ctl.Top, "Top");
+                Ctl.Left = ResponsiveObj.GetMetrics(Ctl.Left, "Left");
+            }
+        }
+
+        private void SetupGrid()
+        {
+            try
+            {
+                // http://stackoverflow.com/questions/64041/winforms-datagridview-font-size
+                ResponsiveDataGridView.DefaultCellStyle.Font = new Font(FontFamily.GenericSansSerif, ResponsiveObj.GetMetrics(15), FontStyle.Regular);
+                ResponsiveDataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+                ResponsiveDataGridView.RowTemplate.Height = ResponsiveObj.GetMetrics(30);
+                ResponsiveDataGridView.Columns.Add("", "Word");
+                ResponsiveDataGridView.Columns[0].HeaderCell.Style.Font = new Font(FontFamily.GenericSansSerif, ResponsiveObj.GetMetrics(15), FontStyle.Regular);
+
+                // https://msdn.microsoft.com/en-us/library/system.windows.forms.datagridview.font%28v=vs.110%29.aspx?f=255&MSPPError=-2147217396
+                // http://stackoverflow.com/questions/5206203/can-i-set-the-max-number-of-rows-in-unbound-datagridview
+                ResponsiveDataGridView.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Raised;
+                ResponsiveDataGridView.CellBorderStyle = DataGridViewCellBorderStyle.Single;
+
+                // http://stackoverflow.com/questions/2154154/datagridview-how-to-set-column-width
+                ResponsiveDataGridView.Columns[0].FillWeight = ResponsiveObj.GetMetrics(200);
+                DataGridViewColumn column = ResponsiveDataGridView.Columns[0];
+                column.Width = ResponsiveObj.GetMetrics(200);
+                column.MinimumWidth = ResponsiveObj.GetMetrics(200);
+                column.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                column.HeaderCell.Style.BackColor = Color.Red;
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show("An error occurred in 'SetupGrid' method of 'LoadWords' form. Error msg: " + Ex.Message);
+            }
+        }
+    }
+}
